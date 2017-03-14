7cb4b37b1e8ccd5f49c9e74862e8ef32f576b765
Pooja Bansal <pban11@sapient.com>
2017-03-15 04:03:08 +0530

    code added

--- Puppetfile
+++ Puppetfile
@@ -7,7 +7,8 @@ forge "http://forge.puppetlabs.com"
 #mod "puppetlabs/concat",      '2.1.0'
 
 mod 'zuinnote-oraclejdk8', '1.0.1'
-
+mod 'puppetlabs-apt', '2.3.0'
+mod 'puppetlabs-stdlib', '4.15.0'
 # Modules from Git
 # Examples: https://github.com/puppetlabs/r10k/blob/master/doc/puppetfile.mkd#examples
 #mod 'apache',
--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,24 +1,24 @@
 #!/usr/bin/env ruby
-begin
-  require 'rugged'
-rescue LoadError => e
-  t = Time.new
-  puts t.to_i
-else
+#begin
+#  require 'rugged'
+#rescue LoadError => e
+ # t = Time.new
+ #puts t.to_i
+#else
 
-  environmentpath = ARGV[0]
-  environment     = ARGV[1]
+ # environmentpath = ARGV[0]
+ # environment     = ARGV[1]
 
-  repo = Rugged::Repository.discover(File.join(environmentpath, environment))
-  head  = repo.head
+  #repo = Rugged::Repository.discover(File.join(environmentpath, environment))
+  #head  = repo.head
 
   #sha1 hash of the newest commit
-  head_sha = head.target_id
+  #head_sha = head.target_id
 
   #the commit message associated the newest commit
-  commit = repo.lookup(head_sha)
+  #commit = repo.lookup(head_sha)
 
   #add something to find the remote url
 
-  puts head_sha 
-end
+  #puts head_sha 
+#end
