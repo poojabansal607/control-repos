c728f3b4638215deba0abf0155a8e39314cb917c
Pooja Bansal <pban11@sapient.com>
2017-03-20 03:05:41 +0530

    code modified

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,12 +33,7 @@ node del2vmpldevop03.sapient.com {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-#include '::mysql::server'
-
-class { '::mysql::server':
-  root_password => 'root123',
-  override_options => { 'mysqld' => { 'max_connections' => '1024' }} 
-  }
+include '::mysql::server'
 
 mysql::db { 'devops_db':
   user => 'root',
