9e85632ce204eeea90f6e96791ca1fbafbc61c24
Pooja Bansal <pban11@sapient.com>
2017-03-24 03:07:30 +0530

    code modified

--- manifests/site.pp
+++ manifests/site.pp
@@ -96,12 +96,16 @@ exec { 'run_my_assessment':
    command => 'java -jar target/assessment-1.0-SNAPSHOT.jar server src/main/resources/devops-assessment.yml',
    path => '/usr/bin',
   # timeout => '10',
+   logoutput => on_failure,
+   loglevel => verbose,
 }
 exec { 'run_my_script':
    cwd => '/etc/puppetlabs/puppet/deploy_files/gs-service',
    command => 'java -jar target/gs-rest-service-cors-0.1.0.jar',
   # logoutput => 'true',
-   path => '/usr/bin',
+   path => '/bin',
   # timeout => '10',
+  logoutput => on_failure,
+  loglevel => verbose,
  }
   }
